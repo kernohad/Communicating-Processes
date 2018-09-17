ba749723f822169f1c34b8334a2409945bc5c78a
Dylan Kernohan <kernohad@mail.gvsu.edu>
2018-09-17 15:15:56 -0400

    Created parentProgram, empty file

---
+++ parentProgram.c
@@ -0,0 +1,14 @@
+#include <stdio.h> 
+#include <stdlib.h> 
+#include <unistd.h> 
+#include <string.h>
+
+#define READ 0 
+#define WRITE 1 
+#define MAX 1024
+
+int main() 
+{	
+	printf("Hello World!"); 
+    return 0; 
+}
