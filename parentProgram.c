59f05ef5684815ca1a61fdf9846942224bf2a1cc
Dylan Kernohan <kernohad@mail.gvsu.edu>
2018-09-18 20:57:27 -0400

    Basic program structure.

--- parentProgram.c
+++ parentProgram.c
@@ -1,14 +1,47 @@
 #include <stdio.h> 
 #include <stdlib.h> 
 #include <unistd.h> 
-#include <string.h>
+#include <signal.h>
 
-#define READ 0 
-#define WRITE 1 
-#define MAX 1024
+void sigHandler (int);
 
+/*********************************************************
+ * A program to learn about interprocess communication.
+ * The parent process waits for signals sent by the child.
+ *
+ * Dustin Thurston & Dylan Kernohan
+ *********************************************************/
 int main() 
 {	
-	printf("Hello World!"); 
+	printf("Hello World!\n"); 
+
+    signal (SIGUSR1, sigHandler1);
+    signal (SIGUSR2, sighandler2);
+
+    printf ("waiting...\n");
+    pause();
     return 0; 
+}
+
+
+void sigHandler1 (int sigNum){
+
+    printf(" received an interrupt.\n");
+
+    // This is where shutdown code would be insrted
+    
+    sleep(1);
+    printf("outta here.\n");
+    exit(0);
+}
+
+void sigHandler2 (int sigNum){
+
+    printf(" received an interrupt.\n");
+
+    // This is where shutdown code would be insrted
+    
+    sleep(1);
+    printf("outta here.\n");
+    exit(0);
 }
